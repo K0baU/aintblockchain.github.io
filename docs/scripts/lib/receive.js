96a28b6df989c604280a09080a25c20100f0e527
K0baU <63403741+K0baU@users.noreply.github.com>
2023-11-23 13:01:39 +0900

    Add files via upload

--- docs/scripts/lib/receive.js
+++ docs/scripts/lib/receive.js
@@ -5,52 +5,53 @@ const config = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
 const mimes = {};
 
 const setupConn = (id, pub, description) => {
-                    if (description && description.type == "answer") {
-                        conns[id].setRemoteDescription(description);
-                        return;
-                    }
-                    // Create the local connection and its event listeners
-                    const con = new RTCPeerConnection(config);
-                    con.onconnectionstatechange = () => {
-                        log(con.connectionState);
-                        switch (con.connectionState) {
-                            case "connected":
-                                if (creditOuts[id]) {
-                                    onlines[id].textContent = onlineMsg;
-                                } else {
-                                    const newPeer = { id, name: "", credit: 0 };
-                                    opr.crud({ store: "peers", op: "add", rec: newPeer, callback: showAPeer });
-                                }
-                                break;
-                            case "disconnected":
-                                onlines[id].textContent = "";
-                                break;
+    if (description && description.type == "answer") {
+        conns[id].setRemoteDescription(description);
+        return;
+    }
+    // Create the local connection and its event listeners
+    const con = new RTCPeerConnection(config);
+    con.onconnectionstatechange = () => {
+        log(con.connectionState);
+        switch (con.connectionState) {
+            case "connected":
+                if (creditOuts[id]) {
+                    onlines[id].textContent = onlineMsg;
+                } else {
+                    const newPeer = { id, name: "", credit: 0 };
+                    opr.crud({ store: "peers", op: "add", rec: newPeer, callback: showAPeer });
+                }
+                break;
+            case "disconnected":
+                onlines[id].textContent = "";
+                break;
+        }
+    };
+    // Set up the ICE candidates for the two peers
+    con.onicecandidate = async e => {
+        log("ice");
+        if (!e.candidate) {
+            socketSend({
+                type: "transport", body: {
+                    payload: {
+                        type: "description", body: {
+                            descriptionStr: JSON.stringify(con.localDescription),
+                            sign: Array.from(new Uint8Array(await crypto.subtle.sign(
+                                {
+                                    name: "ECDSA",
+                                    hash: { name: "SHA-384" },
+                                },
+                                user.privateKey,
+                                (new TextEncoder()).encode(JSON.stringify(con.localDescription)),
+                            ))),
+                            pub
                         }
-                    };
-                    // Set up the ICE candidates for the two peers
-                    con.onicecandidate = async e => {
-                        log("ice");
-                        if (!e.candidate) {
-                            socketSend({
-                                type: "transport", body: {
-                                    payload: {
-                                        type: "description", body: {
-                                            descriptionStr: JSON.stringify(con.localDescription),
-                                            sign: Array.from(new Uint8Array(await crypto.subtle.sign(
-                                                {
-                                                    name: "ECDSA",
-                                                    hash: { name: "SHA-384" },
-                                                },
-                                                user.privateKey,
-                                                (new TextEncoder()).encode(JSON.stringify(con.localDescription)),
-                                            ))),
-                                            pub
-                                        }
-                                    }, to: id
-                                }
-                            });
-                        }
-                    }
+                    }, to: id
+                }
+            });
+        }
+    }
+};
 
 export const receive = async (e, pub) => {
     const myId = pub.x + pub.y;
