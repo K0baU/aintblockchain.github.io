eb5f620cf29c64609119089609dc5f9805fce86e
K0baU <63403741+K0baU@users.noreply.github.com>
2023-12-23 00:45:17 +0900

    update docs/scripts/peer/show-peers.js

--- docs/scripts/peer/show-peers.js
+++ docs/scripts/peer/show-peers.js
@@ -3,6 +3,6 @@ import { opr } from "../lib/db.js";
 
 opr.for({ store: "peers", f: value => {
     if (value.name == "" && value.credit == 0) {
-        opr.crud({ store: "peers", op: "delete" });
+        opr.crud({ store: "peers", op: "delete", rec: value.id });
     }
 }, end: () => opr.for({ store: "peers", f: showAPeer }) });
