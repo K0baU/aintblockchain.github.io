1d28e6d56c0eafb3d4777ce602cfd32c7da8dc30
K0baU <63403741+K0baU@users.noreply.github.com>
2023-12-23 01:26:59 +0900

    remove setupWs

--- docs/scripts/lib/connect.js
+++ docs/scripts/lib/connect.js
@@ -1,35 +1,58 @@
-import { log } from "./log.js";
-import { doc } from "./doc.js";
-import { opr } from "./db.js";
-import { setupWs } from "./socket.js";
-import { cid } from "../content/id.js";
-import { showAPeer } from "../peer/show-a-peer.js";
-import { addContent } from "../content/add.js";
+import {
+    log
+} from "./log.js";
+import {
+    doc
+} from "./doc.js";
+import {
+    opr
+} from "./db.js";
+import {
+    socketSend
+} from "./socket.js";
+import {
+    cid
+} from "../content/id.js";
+import {
+    showAPeer
+} from "../peer/show-a-peer.js";
+import {
+    addContent
+} from "../content/add.js";
 
 export let user;
-opr.for({ store:"keypairs", f: rec => user = rec, end: async () => {
-    let isNew;
-    if (!user) {                
-        user = await crypto.subtle.generateKey(
-            {
-                name: "ECDSA",
-                namedCurve: "P-384",
-            },
-            true,
-            ["sign", "verify"],
-        );
-        isNew = true;
-    }
-    crypto.subtle.exportKey("jwk", user.publicKey).then(pub => {
-        const myId = pub.x + pub.y;
-        if (isNew)
-            opr.crud({
+export let sendName=()=>{};
+opr.for({
+    store: "keypairs", f: rec => user = rec, end: async () => {
+        let isNew;
+        if (!user) {
+            user = await crypto.subtle.generateKey(
+                {
+                    name: "ECDSA",
+                    namedCurve: "P-384",
+                },
+                true,
+                ["sign", "verify"],
+            );
+            isNew = true;
+        }
+        crypto.subtle.exportKey("jwk", user.publicKey).then(pub => {
+            const myId = pub.x + pub.y;
+            if (isNew)
+                opr.crud({
                 store: "keypairs",
                 op: "add",
-                rec: { body: user, id: myId }
+                rec: {
+                    body: user, id: myId
+                }
             });
-        doc("idSummary").append(myId.slice(0, 4) + "...");
-        doc("idDetails").append(myId);
-        setupWs(pub);
-    });
-} });
+            doc("idSummary").append(myId.slice(0, 4) + "...");
+            doc("idDetails").append(myId);
+            sendName = ()=> {
+                socketSend({
+                    type: "id", body: pub.x + pub.y
+                });
+            }
+        });
+    }
+});
